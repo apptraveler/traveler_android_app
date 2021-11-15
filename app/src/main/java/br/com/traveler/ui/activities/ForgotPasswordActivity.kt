ffaaf0ec5fc47375c80f4fe35778ca7cbe4f0f49
Igor Duarte <igor.duartematos@gmail.com>
2021-11-15 17:26:23 -0300

    feat: Create email validation fn

--- app/src/main/java/br/com/traveler/ui/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/br/com/traveler/ui/activities/ForgotPasswordActivity.kt
@@ -6,6 +6,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import br.com.traveler.R
+import android.text.TextUtils
+import android.util.Patterns
+
 
 class ForgotPasswordActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +19,18 @@ class ForgotPasswordActivity: AppCompatActivity() {
         recoverPassword()
     }
 
+    fun isValidEmail(target: CharSequence?): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+
     fun recoverPassword() {
         val recoverButton = findViewById<AppCompatButton>(R.id.recover_password_button)
         recoverButton.setOnClickListener {
-            val emailEditText = findViewById<EditText>(R.id.forgot_email)
+            val emailEditText = findViewById<EditText>(R.id.forgot_email).text.toString().trim()
+            val isValidEmail = isValidEmail(emailEditText)
             val toastSuccess = Toast.makeText(this, "E-mail de recuperação enviado", Toast.LENGTH_LONG)
             val toastRequired = Toast.makeText(this, "O campo E-mail está inválido ou em branco", Toast.LENGTH_LONG)
-            if(emailEditText.text.toString().trim().isNotEmpty()) {
+            if(isValidEmail) {
                 toastSuccess.show()
             } else {
                 toastRequired.show()
