1507d82314cf1e95c8088905402c23ce89d96de7
Gabriel Guedes <gabrielgdc15@gmail.com>
2021-11-21 23:46:43 -0300

    profile page

---
+++ app/src/main/java/br/com/traveler/models/DestinationAverageCostInformation.kt
@@ -0,0 +1,7 @@
+package br.com.traveler.models
+
+data class DestinationAverageCostInformation(
+    val fiveDays: Double,
+    val sevenDays: Double,
+    val fifteenDays: Double
+)
---
+++ app/src/main/java/br/com/traveler/models/PlaceOfInterest.kt
@@ -0,0 +1,7 @@
+package br.com.traveler.models
+
+data class PlaceOfInterest(
+    val image: String,
+    val name: String,
+    val address: Address
+)
